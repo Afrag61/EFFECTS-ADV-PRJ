87c23acaee91210b2107668a443469aedeca753e
Ahmed Frag <a.frag166@gmail.com>
2024-12-19 17:23:09 +0200

    Editing Quiz Component Added questions.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Components/Header";
+import Quiz from "./Components/Quiz";
 
 function App() {
     return(
         <>
             <Header />
+            <Quiz />
         </>
     )
 }
