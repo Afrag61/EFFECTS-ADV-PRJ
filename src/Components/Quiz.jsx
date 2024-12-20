7b6f2e1dd16b6bb3581a921b920bd41e85d3ff8a
Ahmed Frag <a.frag166@gmail.com>
2024-12-20 13:23:47 +0200

    Randomize question answers & display after questions ends 'questions completed'

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
 import QUESTIONS from './../questions.js'
+import quizComplete from './../assets/quiz-complete.png'
 
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([])
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAnswers(prev => [...prev, selectedAnswer])
     }
+
+    if(quizIsComplete){
+        return <div id="summary">
+            <img src={quizComplete} alt="Trophy icon" />
+            <h2>Quiz Completed</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5)
 
     return (
         <>
@@ -17,7 +29,7 @@ const Quiz = () => {
                 <div className="question">
                     <h3>{QUESTIONS[activeQuestionIndex].text}</h3>
                     <ul className="answers">
-                        {QUESTIONS[activeQuestionIndex].answers.map((answer) =>
+                        {shuffledAnswers.map((answer) =>
                             <li key={answer} className="answer">
                                 <button onClick={() => handleSelectAnswer(answer)}>
                                     {answer}
